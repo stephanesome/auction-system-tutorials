2efa1a8b19596f2042241a5314bd40fdb6b65eea
stephane <stephanesome@gmail.com>
2022-05-18 13:12:35 -0400

    Initialize branch

---
+++ src/main/kotlin/seg3x02/auctionsystem/AuctionSystemApplication.kt
@@ -0,0 +1,11 @@
+package seg3x02.auctionsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuctionSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuctionSystemApplication>(*args)
+}
