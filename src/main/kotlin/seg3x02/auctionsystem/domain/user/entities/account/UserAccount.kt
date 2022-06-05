484dbd2aca906dbbdc45287d71cfe344bb6b6591
stephane <stephanesome@gmail.com>
2022-06-05 13:28:42 -0400

    Core Domain Initial

--- src/main/kotlin/seg3x02/auctionsystem/domain/user/entities/account/UserAccount.kt
+++ src/main/kotlin/seg3x02/auctionsystem/domain/user/entities/account/UserAccount.kt
@@ -1,4 +1,9 @@
 package seg3x02.auctionsystem.domain.user.entities.account
 
-class UserAccount {
+class UserAccount(
+    val id: String,
+    var firstname: String,
+    var lastname: String,
+    var password: String,
+    var email: String) {
 }
