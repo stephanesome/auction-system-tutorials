eefea2d6304280c0cda3ce21a24c4e056dda7f74
stephane <stephanesome@gmail.com>
2023-09-30 22:06:51 -0400

    Spring upgrade to V3

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Example of Domain Driven Development
+Run all the contracts (features) with command
+```
+gradlew test
+```
+The test reports are generated in folder
+`build/reports/test` open `index.html`
+
+To clean previous test results
+```
+gradlew cleanTest
+```
--- src/test/kotlin/seg3x02/auctionsystem/contracts/CucumberContracts.kt
+++ src/test/kotlin/seg3x02/auctionsystem/contracts/CucumberContracts.kt
@@ -2,11 +2,13 @@ package seg3x02.auctionsystem.contracts
 
 import io.cucumber.junit.platform.engine.Constants
 import org.junit.platform.suite.api.ConfigurationParameter
+import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 
 @Suite
-@SelectClasspathResource("seg3x02/auctionsystem/contracts")
+@IncludeEngines("cucumber")
+@SelectClasspathResource("seg3x02.auctionsystem.contracts")
 @ConfigurationParameter(
     key = Constants.GLUE_PROPERTY_NAME,
     value = "seg3x02.auctionsystem.contracts"
