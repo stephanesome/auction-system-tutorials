33dccce7f3e5de2f240d6a2e103ff13cc96a2966
stephane <stephanesome@gmail.com>
2023-09-30 20:59:24 -0400

    Spring upgrade to V3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.7"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+	id("org.springframework.boot") version "3.1.1"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
 }
 
 group = "seg3x02"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
@@ -19,17 +19,17 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.junit.platform:junit-platform-suite:1.8.2")
+	testImplementation("org.junit.platform:junit-platform-suite:1.10.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.cucumber:cucumber-java8:7.3.3")
-	testImplementation("io.cucumber:cucumber-spring:7.3.3")
-	testImplementation("io.cucumber:cucumber-junit-platform-engine:7.3.3")
+	testImplementation("io.cucumber:cucumber-java8:7.14.0")
+	testImplementation("io.cucumber:cucumber-spring:7.14.0")
+	testImplementation("io.cucumber:cucumber-junit-platform-engine:7.14.0")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
 
